f68d5920b1c089739fb8df01ac2ac478657f16e2
Rhys Campbell <rhysevancampbell@gmail.com>
2012-02-22 23:15:19 +0000

    Commiting the modified files

--- runner_data.rb
+++ runner_data.rb
@@ -1,27 +1,42 @@
 require 'date'
 require './pace_time.rb'
+require './EasyPace.rb'
+require './ShortPace.rb'
+require './MediumPace.rb'
+require './LongPace.rb'
 
 class RunnerData
  attr_reader :race_date
  attr_reader :five_km_time
+ attr_reader :five_km_pace
  attr_reader :marathon_pace
+ attr_reader :short_tempo_pace
+ attr_reader :medium_tempo_pace
+ attr_reader :long_tempo_pace
+ attr_reader :easy_pace
  def initialize(racedate, fivekmtime)
    @race_date = racedate
    @five_km_time = fivekmtime
    fivekmseconds =  five_km_time.to_i
+   seconds =  (fivekmseconds / 5)
+   @five_km_pace = Time.at(seconds).gmtime
    rounding = fivekmseconds % 5 
    key = (fivekmseconds + (5-rounding))
-   puts "fivekmseconds = #{fivekmseconds}"
-   puts "rounding = #{rounding}"
-   puts "key = #{key}"
-   puts "hash count = #{PaceTime.MarathonPace().count}"
+   
    mp_in_sec = PaceTime.MarathonPace()[key]
-   puts "Mp in Secs = #{mp_in_sec}"
    @marathon_pace = Time.at(mp_in_sec).gmtime
- end
- def five_km_pace
-   seconds =  (five_km_time.to_i / 5)
-   return Time.at(seconds).gmtime
+   
+   easy_in_sec = EasyPaceTime.EasyPace()[key]
+   @easy_pace = Time.at(easy_in_sec).gmtime
+   
+   short_in_sec = MediumPaceTime.MediumPace()[key]
+   @short_tempo_pace = Time.at(short_in_sec).gmtime
+   
+   med_in_sec = ShortPaceTime.ShortPace()[key]
+   @medium_tempo_pace = Time.at(med_in_sec).gmtime
+   
+   long_in_sec = LongPaceTime.LongPace()[key]
+   @long_tempo_pace = Time.at(long_in_sec).gmtime
  end
  
  def ShortTempoPace
