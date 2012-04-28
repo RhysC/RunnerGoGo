dd6ab89551148a61d25fb25c647b75f29fbd8f3c
Rhys Campbell <rhysevancampbell@gmail.com>
2012-04-28 11:03:49 +0100

    adding tests to confirm the front page params work with the results page

---
+++ tests/home_tests.rb
@@ -0,0 +1,42 @@
+begin 
+  # try to use require_relative first
+  # this only works for 1.9
+  require_relative '../home.rb'
+rescue NameError
+  # oops, must be using 1.8
+  # no problem, this will load it then
+  require File.expand_path('../home.rb', __FILE__)
+end
+
+require 'test/unit'
+require 'rack/test'
+
+class MyAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+  
+  def test_my_default
+    get '/'
+    assert last_response.ok?
+    assert_match /Welcome to Runner Go Go/, last_response.body
+  end
+
+  def test_plan_with_params
+    get '/racedate/14-05-2012/fivekmtime/21m35s' #, :name => 'Frank'
+    assert last_response.ok?, "response is not ok :("
+    assert_match /Marathon date : Mon 14 May 2012/, last_response.body
+    assert_match /Times are based on the pace of a 5km race at the given pace 04:19/, last_response.body 
+  end
+  
+  def test_all_5km_run_times_between_15_and_30_minutes
+    (15..29).each do |minute| 
+      ((0..59).select {|x| (x % 5) ==0}).each do |second| 
+        get "/racedate/14-05-2012/fivekmtime/#{minute}m#{second}s" #, :name => 'Frank'
+        assert last_response.ok?, "response is not ok :("
+      end
+  	end
+  end
+end
